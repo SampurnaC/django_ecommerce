6a7df1954545438aed726b8d1f390c73d1e89a89
sam <sampurna.chapagainnn@gmail.com>
2024-06-23 00:00:43 +0100

    add view code for product show and category products

--- store/views.py
+++ store/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from . models import Product
+from . models import Product, Category
 
 def home(request):
     products=Product.objects.all()
     context={'products': products}
     return render(request, 'store/home.html', context)
+
+def product_show(request, pk):
+    product = Product.objects.get(id=pk)
+    context = {'product': product}
+    return render(request, 'store/product_show.html', context)
+
+def category_products(request, cat):
+    category = Category.objects.get(name=cat)
+    category_products = category.products.all()
+    context = {'category_products': category_products}
+    return render(request, 'store/category_products.html', context)
