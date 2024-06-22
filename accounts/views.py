484a31b105baf976b3c38bad94519e8a3f9c92c0
sam <sampurna.chapagainnn@gmail.com>
2024-06-22 22:25:46 +0100

    add logout view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+def login(request):
+    return render(request, 'accounts/login.html')
+
+def logout_user(request):
+    print('logged out')
+    logout(request)
+
+    return redirect('store:home')
