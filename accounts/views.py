3c7e0ef397ce2aecb0c6d421da41f2b274850413
sam <sampurna.chapagainnn@gmail.com>
2024-06-22 23:15:39 +0100

    add view code for register

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import UserRegisterForm
 
-def login(request):
-    return render(request, 'accounts/login.html')
+
+def register(request):
+    if request.method=='POST':
+        form=UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username=form.cleaned_data.get('username')
+            return redirect('accounts:login')
+    else:
+        form=UserRegisterForm()    
+    context={'form': form}
+    return render(request, 'accounts/register.html', context)
 
 def logout_user(request):
     print('logged out')
     logout(request)
-
+    messages.success(request, ('Logged out successfully'))
     return redirect('store:home')
