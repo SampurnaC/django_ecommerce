e3feb723871e389509e309b42a83bb74566df16b
sam <sampurna.chapagainnn@gmail.com>
2024-06-22 22:51:36 +0100

    url for login and logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+from .forms import SigninForm
+
+app_name = 'accounts'
 
 urlpatterns = [
-    path('login/', views.login, name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html', authentication_form=SigninForm), name='login'),
+    path('logout/', views.logout_user, name='logout'),
+
 ]
