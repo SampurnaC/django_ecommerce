3bbc73df0e771ebe03aa851a03b78cb33dda7613
sam <sampurna.chapagainnn@gmail.com>
2024-06-22 23:15:18 +0100

    add route for regiser

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from . import views
+from accounts import views as user_views
 from django.contrib.auth import views as auth_views
 from .forms import SigninForm
 
 app_name = 'accounts'
 
 urlpatterns = [
+    path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html', authentication_form=SigninForm), name='login'),
-    path('logout/', views.logout_user, name='logout'),
+    path('logout/', user_views.logout_user, name='logout'),
 
 ]
