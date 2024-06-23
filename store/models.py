230ecd936ab1a58070f7520c1c5db312fbbb9847
sam <sampurna.chapagainnn@gmail.com>
2024-06-23 18:56:05 +0100

    add related_name products to category table

--- store/models.py
+++ store/models.py
@@ -26,7 +26,7 @@ class Customer(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE, default=1)
     description = models.CharField(max_length=250, default='', blank=True, null=True)
     image = models.ImageField(upload_to='uploads/product/')
     is_sale = models.BooleanField(default=False)
