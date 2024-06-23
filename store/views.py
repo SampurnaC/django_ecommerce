9213725277069d5edbe9ac827475baeb3bba63e6
sam <sampurna.chapagainnn@gmail.com>
2024-06-23 18:58:12 +0100

    pass context in views

--- store/views.py
+++ store/views.py
@@ -4,7 +4,8 @@ from . models import Product, Category
 
 def home(request):
     products=Product.objects.all()
-    context={'products': products}
+    categories=Category.objects.all()
+    context={'products': products, 'categories': categories}
     return render(request, 'store/home.html', context)
 
 def product_show(request, pk):
@@ -15,5 +16,5 @@ def product_show(request, pk):
 def category_products(request, cat):
     category = Category.objects.get(name=cat)
     category_products = category.products.all()
-    context = {'category_products': category_products}
+    context = {'category': category, 'category_products': category_products}
     return render(request, 'store/category_products.html', context)
