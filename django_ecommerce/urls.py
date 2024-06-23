fcc6c52091b2a3c419ef3d1657c85d9e510fd831
sam <sampurna.chapagainnn@gmail.com>
2024-06-23 18:57:15 +0100

    include cart url in project url file

--- django_ecommerce/urls.py
+++ django_ecommerce/urls.py
@@ -7,6 +7,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
-    path('users/', include('accounts.urls'))
+    path('users/', include('accounts.urls')),
+    path('cart/', include('cart.urls')),
+    
     
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
