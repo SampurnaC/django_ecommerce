9ab7765f521a754664407a05e5f471d6cd9c922a
sam <sampurna.chapagainnn@gmail.com>
2024-06-25 22:21:35 +0100

    add a context processor to count total quantity added in cart

--- cart/context_processors.py
+++ cart/context_processors.py
@@ -1,5 +1,15 @@
-# from .cart import Cart
 
-# def cart(request):
+def cart_total_quantity(request):
+    total_quantity = 0
+    if 'cart' in request.session:
+        cart = request.session['cart']
+        # total_quantity = sum(item['quantity'] for item in cart.values())
+        total_quantity=0
+        for item in cart.values():
+            quantity=item['quantity']
+            total_quantity=total_quantity+quantity
+            
     
-#     return {'cart': Cart(request)}
+    return {
+        'total_quantity': total_quantity
+    }
